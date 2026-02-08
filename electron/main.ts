4effd5750f3a855ae82720f1f90ff697ba6adaf4
fengjutian <fjutian@foxmail.com>
2026-02-08 18:22:18 +0800

    chore: 更新Next.js构建文件和静态资源

---
+++ electron/main.ts
@@ -0,0 +1,50 @@
+import { app, BrowserWindow, Menu } from 'electron';
+import * as path from 'path';
+import * as url from 'url';
+
+let mainWindow: BrowserWindow | null;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  Menu.setApplicationMenu(null);
+
+  if (app.isPackaged) {
+    mainWindow.loadURL(
+      url.format({
+        pathname: path.join(__dirname, '../out/index.html'),
+        protocol: 'file:',
+        slashes: true,
+      })
+    );
+  } else {
+    mainWindow.loadURL('http://localhost:3000');
+    mainWindow.webContents.openDevTools();
+  }
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', () => {
+  createWindow();
+
+  app.on('activate', function () {
+    if (mainWindow === null) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
